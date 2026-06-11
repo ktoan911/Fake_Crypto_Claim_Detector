26ca75edcebd73097ff954269961d9a895de7ee9
kateee <khanhtoan.forwork@gmail.com>
2026-06-12 00:21:15 +0700

    add

--- scripts/train_lora.py
+++ scripts/train_lora.py
@@ -6,6 +6,9 @@ Required CSV columns: text/claim, evidence, label
 
 import argparse
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dotenv import load_dotenv
 from loguru import logger
